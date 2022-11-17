2bbb841db5b1aaed8659c7898d569612d4bf4964
Zhanibek <zkhodzhabekov@gmail.com>
2022-11-17 19:18:54 +0600

    Creation of main modules:

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,12 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { CoworkingModule } from './coworking/coworking.module';
+import { RoomModule } from './room/room.module';
+import { EventModule } from './event/event.module';
 
 @Module({
+
+  imports: [UserModule, AuthModule, CoworkingModule, RoomModule, EventModule]
 })
 export class AppModule {}
