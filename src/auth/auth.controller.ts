3a8b60e0f84035aee2154b9a63045e5c5f790ab3
Zhanibek <zkhodzhabekov@gmail.com>
2022-11-17 20:26:39 +0600

    small changes in auth controller and service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor (private authService: AuthService){}
+
+    @Post('register')
+    register() {
+        return 'I am registering'
+    }
+
+    @Post('login')
+    login() {
+        return 'I am loggin in'
+    }
+
+    @Get('logout')
+    logout() {
+        
+    }
+}
