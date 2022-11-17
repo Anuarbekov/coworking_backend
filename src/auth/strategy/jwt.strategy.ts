af53bce2457f6465b66630543a97cc04f1eb446a
Zhanibek <zkhodzhabekov@gmail.com>
2022-11-17 23:40:20 +0600

    Ficed validation procces problem

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,12 +1,27 @@
+import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
-import { ignoreElements } from "rxjs";
+import { PrismaService } from "src/prisma/prisma.service";
 
+@Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy){
-    constructor(){
+    constructor(private prisma: PrismaService){
         super({
             jwtFormRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             secretOrKey: '1ju2c78ounx8u3iqsyo8fn43qec4onu3a2ho4c83qcru2nicp9ruiqedjeisg5g85'
         })
+    }
+    async validate(payload:{
+        sub: number,
+        email: string,
+        role: string
+    }){
+        const user = await this.prisma.user.findFirst({
+            where: {
+                id: payload.sub
+            }
+        })
+        return payload;
+
     }
 }
