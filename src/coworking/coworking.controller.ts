66e81e1b4f4f3a5474a5482458d3e2b51059fd6d
Zhanibek <zkhodzhabekov@gmail.com>
2022-11-18 20:55:05 +0600

    Added coworking-controller main code

--- src/coworking/coworking.controller.ts
+++ src/coworking/coworking.controller.ts
@@ -1,17 +1,36 @@
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { CoworkingService } from './coworking.service';
+import { CoworkingDto } from './dto';
 
-@Controller('coworking')
+@Controller('coworkings')
 export class CoworkingController {
+
+    constructor(private coworkingService: CoworkingService){}
 
     @UseGuards(AuthGuard('jwt'))
     @Get()
     getAll(){
-        return "OK";
+        return this.coworkingService.getAll();
     }
 
     @Get(':id')
     get(@Param('id') id: number){
-        
+        return this.coworkingService.get(id);
+    }
+
+    @Post()
+    create(@Body() dto : CoworkingDto){
+        return this.coworkingService.create(id);
+    }
+
+    @Put(':id')
+    update(@Param('id') id: number){
+        return this.coworkingService.update(id);
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: number){
+        return this.coworkingService.delete(id)
     }
 }
