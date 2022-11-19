9a9f1f537874d2380b4aa5781232cf67d2412bde
Zhanibek <zkhodzhabekov@gmail.com>
2022-11-19 17:33:21 +0600

    added room controller and service

--- src/room/room.module.ts
+++ src/room/room.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { RoomController } from './room.controller';
+import { Room } from './room';
 
-@Module({})
+@Module({
+  controllers: [RoomController],
+  providers: [Room]
+})
 export class RoomModule {}
