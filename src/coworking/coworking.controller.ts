e86ec430cfc08f64f98d70d03913241ea8bc2f65
Zhanibek <zkhodzhabekov@gmail.com>
2022-12-08 18:28:08 +0600

    Added Roles and Authentication restriction to coworking.cotroller

--- src/coworking/coworking.controller.ts
+++ src/coworking/coworking.controller.ts
@@ -1,5 +1,7 @@
 import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Roles } from 'src/role/roles.decorator';
+import { RolesGuard } from 'src/role/roles.guard';
 import { CoworkingService } from './coworking.service';
 import { CoworkingDto } from './dto';
 
@@ -8,7 +10,6 @@ import { CoworkingDto } from './dto';
 export class CoworkingController {
 
     constructor(private coworkingService: CoworkingService){}
-    @UseGuards(AuthGuard('jwt'))
     @Get()
     getAll(){
         return this.coworkingService.getAll();
@@ -19,18 +20,22 @@ export class CoworkingController {
         return this.coworkingService.get(id);
     }
 
+    @UseGuards(AuthGuard('jwt'))
+    @Roles('admin')
     @Post()
     create(@Body() dto : CoworkingDto){
         return this.coworkingService.create(dto);
     }
 
+    @UseGuards(AuthGuard('jwt'))
+    @Roles('admin')
     @Put(':id')
     update(@Param('id') id: number, @Body() dto : CoworkingDto){
         return this.coworkingService.update(id, dto);
     }
 
     @UseGuards(AuthGuard('jwt'))
-
+    @Roles('admin')
     @Delete(':id')
     delete(@Param('id') id: number){
         return this.coworkingService.delete(id);
--- src/role/roles.decorator.ts
+++ src/role/roles.decorator.ts
@@ -1,11 +1,3 @@
 import { SetMetadata } from '@nestjs/common';
-import { Role } from './role.enum';
 
-export const ROLES_KEY = 'roles';
-export const Roles = (...roles: Role[]) => SetMetadata(ROLES_KEY, roles);
-
-//roles decorator in nestjs?
-
-
-
-
+export const Roles = (...roles: string[]) => SetMetadata('roles', roles);
