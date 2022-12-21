03cf1a912053098711a3968992b32306103d9cd3
Anuarbekov <mikonek.a@gmail.com>
2022-12-21 20:01:35 +0600

    user.module changed, authmodule added to usermodule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { EventService } from 'src/event/event.service';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
-
+import { forwardRef } from '@nestjs/common/utils';
+import { AuthModule } from 'src/auth/auth.module';
 @Module({
+  imports: [forwardRef(() => AuthModule)],
   controllers: [UserController],
-  providers: [UserService, EventService]
+  providers: [UserService, EventService],
+  exports: [UserService],
 })
 export class UserModule {}
