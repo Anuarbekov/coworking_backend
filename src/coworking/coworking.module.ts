c87dbbf5d332962db14d77c6942f9372c26b53fd
Anuarbekov <mikonek.a@gmail.com>
2022-12-21 20:09:10 +0600

    added authmodule to coworking.module as input

--- src/coworking/coworking.module.ts
+++ src/coworking/coworking.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
-import { RolesGuard } from 'src/auth/role/roles.guard';
 import { CoworkingController } from './coworking.controller';
 import { CoworkingService } from './coworking.service';
-
+import { forwardRef } from '@nestjs/common/utils';
+import { AuthModule } from 'src/auth/auth.module';
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, forwardRef(() => AuthModule)],
   controllers: [CoworkingController],
-  providers: [CoworkingService]
+  providers: [CoworkingService],
 })
 export class CoworkingModule {}
