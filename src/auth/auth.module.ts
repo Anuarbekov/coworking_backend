908f40286f9ff80c1b13d5efa006df269c19db23
Anuarbekov <mikonek.a@gmail.com>
2022-12-21 20:12:05 +0600

    added usermodule to auth.module as import, jwtmodule added

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,15 +1,25 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { UserModule } from 'src/user/user.module';
-import { UserService } from 'src/user/user.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './strategy';
 
 @Module({
-  imports: [PrismaModule, JwtModule.register({}), UserModule],
+  imports: [
+    PrismaModule,
+    JwtModule.register({}),
+    forwardRef(() => UserModule),
+    JwtModule.register({
+      secret: process.env.PRIVATE_KEY || 'SECRET',
+      signOptions: {
+        expiresIn: '24h',
+      },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy, UserService]
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService, JwtModule],
 })
 export class AuthModule {}
