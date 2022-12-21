90a5c480e98e3baf71dcd9db222a2c9963e2a613
Anuarbekov <mikonek.a@gmail.com>
2022-12-21 20:00:24 +0600

    user.dto changed, apiproperty added

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,25 +1,36 @@
-import { IsEmail, IsNotEmpty, IsPhoneNumber, IsString } from "class-validator";
+import { IsEmail, IsNotEmpty, IsPhoneNumber, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger/dist';
+export class UserDto {
+  @ApiProperty({ example: 'user@mail.com', description: 'Почтовый адрес' })
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
 
-export class UserDto{
-    @IsEmail()
-    @IsNotEmpty()
-    email: string;
+  @ApiProperty({ example: '123456789', description: 'Пароль юзера' })
+  @IsString()
+  @IsNotEmpty()
+  password: string;
 
-    @IsString()
-    @IsNotEmpty()
-    password: string;
+  @ApiProperty({ example: 'Kairat', description: 'Имя юзера' })
+  @IsString()
+  @IsNotEmpty()
+  firstname: string;
 
-    firstname: string;
+  @ApiProperty({ example: 'Nurtas', description: 'Фамилия юзера' })
+  @IsString()
+  @IsNotEmpty()
+  lastname: string;
 
-    lastname: string;
+  @ApiProperty({ example: 'ADMIN', description: 'Роль юзера' })
+  @IsNotEmpty()
+  role: string;
 
-    @IsNotEmpty()
-    role: string;
+  @ApiProperty({ example: '+77775355025', description: 'Номер юзера' })
+  @IsPhoneNumber('KZ')
+  @IsNotEmpty()
+  phone: string;
 
-    @IsPhoneNumber('KZ')
-    @IsNotEmpty()
-    phone: string;
-
-    @IsString()
-    telegram: string;
+  @ApiProperty({ example: 'kairat_nurtas', description: 'Телеграм айди юзера' })
+  @IsString()
+  telegram: string;
 }
