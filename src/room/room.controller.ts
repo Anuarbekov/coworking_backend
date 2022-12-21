8460498a632cd2eb3cce263b970ff3f60f5a4147
Anuarbekov <mikonek.a@gmail.com>
2022-12-21 20:03:35 +0600

    added documentation to room.controller

--- src/room/room.controller.ts
+++ src/room/room.controller.ts
@@ -1,40 +1,60 @@
-import { Body, Controller, Get, Param, Post, Put, Delete } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Put,
+  Delete,
+} from '@nestjs/common';
 import { RoomDto } from './dto';
 import { RoomService } from './room.service';
 
+import { ApiTags, ApiResponse, ApiOperation } from '@nestjs/swagger/dist';
+
+@ApiTags('Комнаты')
 @Controller('rooms')
 export class RoomController {
-    constructor(private roomService: RoomService){}
-    @Get()
-    getAll(){
-        return this.roomService.getAll();
-    }
+  constructor(private roomService: RoomService) {}
 
-    @Get(':id')
-    get(@Param('id') id: number){
-        return this.roomService.get(id);
-    }
+  @ApiOperation({ summary: 'Список комнат' })
+  @ApiResponse({ status: 200, type: [RoomDto] })
+  @Get()
+  getAll() {
+    return this.roomService.getAll();
+  }
 
-    @Post()
-    create(@Body() dto: RoomDto){
-        return this.roomService.create(dto);
-    }
+  @ApiOperation({ summary: 'Комната по айди' })
+  @ApiResponse({ status: 200, type: RoomDto })
+  @Get(':id')
+  get(@Param('id') id: number) {
+    return this.roomService.get(id);
+  }
 
-    @Put(':id')
-    update(@Param('id') id: number, @Body() dto : RoomDto){
-        return this.roomService.update(id, dto);
-    }
+  @ApiOperation({ summary: 'Создать комнату' })
+  @ApiResponse({ status: 200, type: RoomDto })
+  @Post()
+  create(@Body() dto: RoomDto) {
+    return this.roomService.create(dto);
+  }
 
-    @Delete(':id')
-    delete(@Param('id') id: number){
-        return this.roomService.delete(id);
-    }
+  @ApiOperation({ summary: 'Изменить комнату' })
+  @ApiResponse({ status: 200, type: RoomDto })
+  @Put(':id')
+  update(@Param('id') id: number, @Body() dto: RoomDto) {
+    return this.roomService.update(id, dto);
+  }
 
-    @Post(':id/photos')
-    createPhoto(@Param('id') id: number){
-    }
+  @ApiOperation({ summary: 'Удалить комнату' })
+  @ApiResponse({ status: 200, type: RoomDto })
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return this.roomService.delete(id);
+  }
 
-    @Delete(':id/photos:photoId')
-    deletePhoto(@Param('photoId') id: number){
-    }
+  @Post(':id/photos')
+  createPhoto(@Param('id') id: number) {}
+
+  @Delete(':id/photos:photoId')
+  deletePhoto(@Param('photoId') id: number) {}
 }
