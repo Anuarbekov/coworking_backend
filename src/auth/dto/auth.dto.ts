3ad8415835c8cbb30f98074cb5d47195d9c2dd21
Anuarbekov <mikonek.a@gmail.com>
2022-12-21 20:14:27 +0600

    documentation to auth.dto

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,14 +1,16 @@
-import {IsNotEmpty, IsPhoneNumber, IsString } from "class-validator"
+import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger/dist';
 
+export class LoginDto {
+  @ApiProperty({
+    example: 'user@mail.com or 87775355025',
+    description: 'Почтовый адрес или телефон юзера',
+  })
+  @IsNotEmpty()
+  emailOrPhone: string;
 
-export class LoginDto{
-
-    @IsNotEmpty()
-    @IsNotEmpty()
-    emailOrPhone: string;
-
-    @IsString()
-    @IsNotEmpty()
-    password: string;
-
+  @ApiProperty({ example: '123456789', description: 'Пароль юзера' })
+  @IsString()
+  @IsNotEmpty()
+  password: string;
 }
