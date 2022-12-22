a02045d7e91e26c727ba14d1a92d9aad3cd88ce9
Anuarbekov <mikonek.a@gmail.com>
2022-12-22 13:12:05 +0600

    add guards and bearerauth to room.controller

--- src/room/room.controller.ts
+++ src/room/room.controller.ts
@@ -7,17 +7,28 @@ import {
   Put,
   Delete,
   ParseIntPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { RoomDto } from './dto';
 import { RoomService } from './room.service';
 
-import { ApiTags, ApiResponse, ApiOperation } from '@nestjs/swagger/dist';
+import {
+  ApiTags,
+  ApiResponse,
+  ApiOperation,
+  ApiBearerAuth,
+} from '@nestjs/swagger/dist';
+import { Roles } from 'src/auth/role/roles.decorator';
+import { RolesGuard } from 'src/auth/role/roles.guard';
 
 @ApiTags('Комнаты')
 @Controller('rooms')
 export class RoomController {
   constructor(private roomService: RoomService) {}
 
+  @ApiBearerAuth()
+  @Roles('USER', 'ADMIN')
+  @UseGuards(RolesGuard)
   @ApiOperation({ summary: 'Список комнат' })
   @ApiResponse({ status: 200, type: [RoomDto] })
   @Get()
@@ -32,6 +43,8 @@ export class RoomController {
     return this.roomService.get(id);
   }
 
+  @ApiBearerAuth()
+  @Roles('ADMIN')
   @ApiOperation({ summary: 'Создать комнату' })
   @ApiResponse({ status: 200, type: RoomDto })
   @Post()
@@ -39,6 +52,8 @@ export class RoomController {
     return this.roomService.create(dto);
   }
 
+  @ApiBearerAuth()
+  @Roles('ADMIN')
   @ApiOperation({ summary: 'Изменить комнату' })
   @ApiResponse({ status: 200, type: RoomDto })
   @Put(':id')
@@ -46,6 +61,8 @@ export class RoomController {
     return this.roomService.update(id, dto);
   }
 
+  @ApiBearerAuth()
+  @Roles('ADMIN')
   @ApiOperation({ summary: 'Удалить комнату' })
   @ApiResponse({ status: 200, type: RoomDto })
   @Delete(':id')
